523532b952b37aa690c97e11c4fd7e0e6d8f2cda
gleyder <gleydergt@gmail.com>
2023-05-10 18:10:49 -0500

    se crean php

--- php/captura.php
+++ php/captura.php
@@ -1,20 +1,8 @@
-<docttype html>
-    <!DOCTYPE html>
-    <html lang="en">
-
-    <head>
-        <title>
-            Captura
-        </title>
-    </head>
-
-    <body>
-        <?php
-        $conexion = mysqli_connect("localhost", "phpmyadmin", "1619*", "senaphp") or die("Problems con la conexion a la BD");
-        echo "Conectado a la base de datos";
-        mysqli_query($conexion, "insert into aprendiz(nombres,apellidos,ficha) values
-('$_REQUEST[nombres]','$_REQUEST[apellido]',$_REQUEST[ficha])") or die("Problemas en el select" . mysqli_error($conexion));
-        ?>
-    </body>
-
-    </html>
+<?php
+$conexion=mysqli_connect("localhost","gleyder","1619*","senaphp");
+mysqli_query($conexion, "insert into aprendiz(nombres,apellidos,ficha) values
+('$_REQUEST[nombres]','$_REQUEST[apellidos]',$_REQUEST[ficha])") or die
+("Problemas en el select" . mysqli_error($conexion));
+mysqli_close($conexion);
+echo "Registro exitoso";
+?>
